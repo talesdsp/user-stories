f15a3c15c26727ebe896edcd9bd4f9b725eb66dc
tales <talesdsp@gmail.com>
2020-07-02 23:44:52 -0300

    authenticate user

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const router = require("express").Router()
+const passport = require("passport")
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }))
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/dashboard")
+  }
+)
+
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/")
+})
+
+module.exports = router
