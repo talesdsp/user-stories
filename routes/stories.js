ae0edb03a2ea8fb1bd82f92d441b040f9ceba815
tales <talesdsp@gmail.com>
2020-07-02 23:49:46 -0300

    create and get stories

---
+++ routes/stories.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const router = express.Router()
+const { ensureAuth } = require("../middleware/auth")
+
+const Story = require("../models/Story")
+
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("stories/add")
+})
+
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id
+    await Story.create(req.body)
+    res.redirect("/dashboard")
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
+  }
+})
+
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ created_at: "desc" })
+      .lean()
+
+    res.render("stories/index", stories)
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
+  }
+})
+
+module.exports = router
