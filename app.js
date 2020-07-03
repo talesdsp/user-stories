0c177a438599e1bf4afc00b6fb7790db75ac37eb
tales <talesdsp@gmail.com>
2020-07-03 01:00:17 -0300

    fix typo

--- app.js
+++ app.js
@@ -47,16 +47,13 @@ app.use(express.json())
 
 // Method Override
 app.use(
-  methodOverride(
-    (req,
-    (res) => {
-      if (req.body && typeof req.body === "object" && "_method" in req.body) {
-        let method = req.body._method
-        delete req.body._method
-        return method
-      }
-    })
-  )
+  methodOverride((req, res) => {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
 )
 
 // Logger
